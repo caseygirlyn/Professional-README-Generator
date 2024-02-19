43fa67362b8ac7baa26522dc4bf6d3212fd73508
Girlyn Antillon <casey.girlyn@gmail.com>
2024-02-19 22:05:15 +0000

    list array of questions for user

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is your project\'s name?',
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of you project',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'None']
+    },
+    {
+        type: 'input',
+        message: 'What kind of command should be run to install dependencies?',
+        name: 'cmdInstall',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'cmdTest'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'useRepo'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contribution'
+    },
 ];
 
 // function to write README file
